b89ffc8fee7a090636a7b35424a8ed6b6781be0d
Satinder <development.geektech@gmail.com>
2026-07-22 16:28:23 +0530

    scout talent code  structure

--- android/app/src/main/kotlin/com/scouttalent/scouttalent2/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.scoutTalent
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-//class MainActivity : FlutterActivity()
-class MainActivity: FlutterFragmentActivity() {
-}
